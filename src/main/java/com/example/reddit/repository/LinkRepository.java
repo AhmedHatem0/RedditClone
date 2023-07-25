3e8d11a9d0f110ab95b8498a0d5f626536fbce6a
AhmedHatem0 <ahmedhateem315@gmail.com>
2023-07-25 11:50:43 +0300

    added comment and link repositories

---
+++ src/main/java/com/example/reddit/repository/LinkRepository.java
@@ -0,0 +1,8 @@
+package com.example.reddit.repository;
+
+import com.example.reddit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link,Long> {
+
+}
