6bfa03ccf0faf8b1277c0164c45a83ee2e14dc16
AhmedHatem0 <ahmedhateem315@gmail.com>
2023-07-25 11:32:06 +0300

    entities refactored with lombok @data, @noArgsConstructor

--- src/main/java/com/example/reddit/domain/Link.java
+++ src/main/java/com/example/reddit/domain/Link.java
@@ -1,11 +1,17 @@
 package com.example.reddit.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
+@Data
+@NoArgsConstructor
+
 public class Link {
     @Id
     @GeneratedValue
@@ -13,52 +19,4 @@ public class Link {
     private String title;
     private String url;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Link() {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Link link = (Link) o;
-        return Objects.equals(id, link.id) && Objects.equals(title, link.title);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title);
-    }
-
-    @Override
-    public String toString() {
-        return "Link{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-
-                '}';
-    }
 }
